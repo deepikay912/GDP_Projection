2ed0ae13d822da1195e5bb69e82140062e509236
deepikay912 <deepika640yannamani@gmail.com>
2019-04-30 22:52:13 -0700

    bar chart color decoration and labels on each bar
    bar chart color decoration and labels on each bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,26 @@
 
 import React, { Component } from 'react';
 import './App.css';
-
 import BarChart from './BarChart';
-import {rooturl} from './config/settings';
-import axios from 'axios';
-
-//App Component
-// class App extends Component {
-//   render() {
-//     return (
-//       //Use Browser Router to route to different pages
-//       <BrowserRouter>
-//         <div>
-//           {/* App Component Has a Child Component called Main*/}
-//           <Main/>
-//         </div>
-//       </BrowserRouter>
-//     );
-//   }
-// }
-//Export the App component so that it can be used in index.js
-
 
 
 class App extends Component {
   
   state = {
-    data: [],
     width: 700,
     height: 500,
-    id : 100
-  }
-
-  componentDidMount() {
-  
-    axios.get('http://'+rooturl+':3001/api/graph')
-     .then(response => {
-
-      console.log(response.data);
-
-              this.setState({
-                  data : this.state.data.concat(response.data)
-              
-                 });
-       });
-       
-  }
+   }
 
   render() {
     return (
+      <div>
       <div className="App">
       <h3 align="center"> United States GDP Annual Growth Rate</h3>
-        <BarChart data={this.state.data} width={this.state.width} height={this.state.height} />
+        <BarChart class= "barchart" width={this.state.width} height={this.state.height} />
+
+     
       </div>
+</div>
     );
   }
 }
--- frontend/src/BarChart.js
+++ frontend/src/BarChart.js
@@ -1,139 +1,29 @@
-// import React, { Component } from 'react';
-// import './App.css';
-
-// import * as d3 from "d3";
-// import {rooturl} from './config/settings';
-// import axios from 'axios';
-
-
-// class BarChart extends Component {
-//     componentDidMount() {
-//       this.drawChart();
-//     }
-      
-//     drawChart() {
-
-//         var data = [];
-
-//         axios.get('http://'+rooturl+':3001/api/graph')
-//         .then(response => {
-   
-//          console.log(response.data);
-   
-//                  data = response.data;
-//                  console.log("inside bar chart");
-//                  console.log(data);
-          //        var svgWidth = 1800, svgHeight = 400;
-          //        var margin = { top: 30, right: 20, bottom: 30, left: 150 };
-          //        var width = svgWidth - margin.left - margin.right;
-          //        var height = svgHeight - margin.top - margin.bottom;
-                 
-          //        const svg = d3.select("body").append("svg")
-          //    .attr("width", width)
-          //    .attr("height", this.props.height);
-           
-          //  // var svg = d3.select('svg')
-          //  // .attr("width", svgWidth)
-          //  // .attr("height", svgHeight);
-           
-          //  var g = svg.append("g")
-          //     .attr("transform", 
-          //        "translate(" + margin.left + "," + margin.top + ")"
-          //     );
-           
-//               var x = d3.scaleTime().rangeRound([0, width]);
-//            var y = d3.scaleLinear().rangeRound([height, 0]);
-           
-//            var line = d3.line()
-//               .x(function(d) { return x(d.date)})
-//               .y(function(d) { return y(d.value)})
-//               x.domain(d3.extent(data, function(d) { return parseInt(d.date) }));
-//               y.domain(d3.extent(data, function(d) { return parseInt(d.value) }));
-           
-//               g.append("g")
-//               .attr("transform", "translate(0," + height + ")")
-//               .call(d3.axisBottom(x))
-//               .select(".domain")
-//               .remove();
-           
-           
-//               g.append("g")
-//               .call(d3.axisLeft(y))
-//               .append("text")
-//               .attr("fill", "#0000")
-//               .attr("transform", "rotate(-90)")
-//               .attr("y", 6)
-//               .attr("dy", "1em")
-//               .attr("text-anchor", "end")
-//               .text("GDP");
-           
-           
-//               g.append("path")
-//            .datum(data)
-//            .attr("fill", "none")
-//            .attr("stroke", "steelblue")
-//            .attr("stroke-linejoin", "round")
-//            .attr("stroke-linecap", "round")
-//            .attr("stroke-width", 1.5)
-//            .attr("d", line);
-           
-//           });
-     
-
-     
-
-
-
-
-
-//     //   svg.selectAll("rect")
-//     //     .data(data)
-//     //     .enter()
-//     //     .append("rect")
-//     //     .attr("x", (d, i) => i * 70)
-//     //     .attr("y", (d, i) => this.props.height - 10 * d)
-//     //     .attr("width", 65)
-//     //     .attr("height", (d, i) => d * 10)
-//     //     .attr("fill", "green")
-
- 
-
-    
-
-//     }
-          
-//     render(){
-//       return <div id={"#" + this.props.id}></div>
-//     }
-//   }
-      
-//   export default BarChart;
-
-
-
-  import React, { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
-
 import * as d3 from "d3";
 import {rooturl} from './config/settings';
 import axios from 'axios';
 
 
 class BarChart extends Component {
-    componentDidMount() {
-      this.drawChart();
 
+    componentDidMount() {
+      
+      this.drawChart();
       this.addSymbols = this.addSymbols.bind(this);
     }
 
 
+    //  adding suffixes for large numbers
     addSymbols(value){
       var suffixes = ["", "K", "M", "B","T"];
       var order = Math.max(Math.floor(Math.log(value) / Math.log(1000)), 0);
       if(order > suffixes.length - 1)
         order = suffixes.length - 1;
       var suffix = suffixes[order];
-      return String((value / Math.pow(1000, order)) + suffix);
+      
+
+      return String(Math.round(value / Math.pow(1000, order) * 100) / 100+ suffix);
     }
       
     drawChart() {
@@ -143,110 +33,127 @@ class BarChart extends Component {
         axios.get('http://'+rooturl+':3001/api/graph')
         .then(response => {
    
-         console.log(response.data);
-   
-                 data = response.data;
-                 data.reverse();
-                 console.log("inside bar chart");
-                 var svgWidth = 1800, svgHeight = 400;
-                 var margin = { top: 30, right: 10, bottom: 30, left: 70 };
-                 var width = svgWidth - margin.left - margin.right;
-                 var height = svgHeight - margin.top - margin.bottom;
-                 
-                 const svg = d3.select("body").append("svg")
-             .attr("width", width+100)
-             .attr("height", this.props.height);
-           
-           // var svg = d3.select('svg')
-           // .attr("width", svgWidth)
-           // .attr("height", svgHeight);
-     
-           var g = svg.append("g")
-              .attr("transform", 
-                 "translate(" + margin.left + "," + margin.top + ")"
-              );
-                 var x = d3.scaleBand()
-                 .rangeRound([0, width])
-                 .padding(0.2);
-               
-               var y = d3.scaleLinear()
-                 .rangeRound([height, 0]);
-
-                 x.domain(data.map(function (d) {
-                  return d.date;
-                }));
-
-              
-
-                y.domain([0, d3.max(data, function (d) {
-                 return d.value;
-                })]);
-
-
-
-	g.append("g")
-	.attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x).tickValues(x.domain().filter(function(d,i){ return !(i%5)})))
-  
-
-	g.append("g")
-  .call(d3.axisLeft(y).tickFormat(function(value) {
-    var suffixes = ["", "K", "M", "B","T"];
-    var order = Math.max(Math.floor(Math.log(value) / Math.log(1000)), 0);
-    if(order > suffixes.length - 1)
-      order = suffixes.length - 1;
-    var suffix = suffixes[order];
-    return String((value / Math.pow(1000, order)) + suffix); 
-  }))
-	.append("text")
-	.attr("fill", "#000")
-	.attr("transform", "rotate(-90)")
-	.attr("y", 6)
-	.attr("dy", "0.71em")
-	.attr("text-anchor", "end")
-  .text("GDP");
-  
-
-  g.selectAll(".bar")
-	.data(data)
-	.enter().append("rect")
-	.attr("class", "bar")
-	.attr("x", function (d) {
-		return x(String(d.date));
-	})
-	.attr("y", function (d) {
-		return y(d.value);
-	})
-	.attr("width", x.bandwidth())
-	.attr("height", function (d) {
-		return height - y(Number(d.value));
-	});
+          data = response.data;
+          data.reverse();
           
+          let svgWidth = 2200, svgHeight = 400;
+          let margin = { top: 30, right: 10, bottom: 30, left: 70 };
+          let width = svgWidth - margin.left - margin.right;
+          let height = svgHeight - margin.top - margin.bottom;
+          const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+          
+          const svg = d3.select("body").append("svg")
+           .attr("width", width+100)
+           .attr("height", this.props.height);
 
 
-svg.append('text')
+              var g = svg.append("g")
+            .attr("transform", 
+                "translate(" + margin.left + "," + margin.top + ")"
+            );
+                var x = d3.scaleBand()
+                .rangeRound([0, width])
+                .padding(0.2);
+              
+              var y = d3.scaleLinear()
+                .rangeRound([height, 0]);
+
+                x.domain(data.map(function (d) {
+                return d.date;
+              }));
+
+            
+
+              y.domain([0, d3.max(data, function (d) {
+                return d.value;
+              })]);        
+
+
+
+        g.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).tickValues(x.domain().filter(function(d,i){ return !(i%5)})))
+        
+
+        g.append("g")
+        .call(d3.axisLeft(y).tickFormat(
+           (value) => {
+             return this.addSymbols(value)
+           }
+        ))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("GDP");
+
+
+        svg.append('text')
     .attr('x', -180)
     .attr('y', 30)
     .attr('transform', 'rotate(-90)')
     .attr('text-anchor', 'middle')
     .text('Annual GDP value in US -->')
 
-svg.append('text')
-    .attr('x', 680)
-    .attr('y', 420)
-    .attr('text-anchor', 'middle')
-    .text('By Year -->')
-       
+        svg.append('text')
+        .attr('x', 680)
+        .attr('y', 420)
+        .attr('text-anchor', 'middle')
+        .text('By Year -->')
+
+        
+
+        g.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) {
+          return x(String(d.date));
+        })
+        .attr("y", function (d) {
+          return y(d.value);
+        })
+        .attr("width", x.bandwidth())
+        .attr("height", function (d) {
+          return height - y(Number(d.value));
+        })
+        .style('fill', (d, i) => colorScale(i));
 
 
-            });
-           
-        
-        
+
+        svg.selectAll('.bar-label')
+        .data(data)
+        .enter()
+        .append('text')
+        .classed('bar-label', true)
+        .attr('x', d => x(d.date) + x.bandwidth()/2+70 )
+        .attr('dx', 0)
+        .attr('y', d => y(d.value)+20)
+        .attr('dy', 0)
+        .text(d => this.addSymbols(d.value)
+          
+        );
+                
+
+
+
+   
+
+      
+  }, (error) => {
+    console.log("Cannot fetch data")
+  }).catch((error) => {
+    console.log(`Error : ${error}`)
+  });
 }
           
     render(){
-      return <div id={"#" + this.props.id}></div>
+      return ( 
+        
+      <div ></div>
+      )
     }
   }
       
